85ff9ee6a8aa2fa424e7a2e05b876b9ff474c0e0
Davranbek <davran9559@gmail.com>
2023-10-25 23:50:17 +0500

    add header style

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import "./style.scss";
 
 const index = () => {
     return (
-        <footer className="bg-slate-400">
+        <footer className="bg-slate-200">
             <div className="mx-auto px-5">
                 <div className="flex items-center justify-center h-[60px]">
                     <h2>Copyright © 2023 Food Warehouse</h2>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,11 +4,11 @@ import "./style.scss";
 
 const index = () => {
     return (
-        <header className="bg-slate-400">
+        <header className="bg-slate-200">
            <div className="mx-auto px-5">
                 <nav className="flex items-center justify-between h-[60px]">
-                    <NavLink to="/">W.H</NavLink>
-                    <NavLink to="/admin" className="uppercase">Admin dashboard</NavLink>
+                    <NavLink to="/" className="text-2xl font-['InterBold'] text-green-600">F.W</NavLink>
+                    <NavLink to="/admin" className="uppercase font-['InterMedium'] border border-green-400 p-2 rounded-lg bg-green-600 text-white">Admin dashboard</NavLink>
                 </nav>
             </div>
         </header>
